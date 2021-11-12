d38aaa6dd1f2709e8bfb3ed261626ee25523e0d2
Shraddha Shinde <shraddha.s@FinIQ-SP.com>
2021-11-12 11:24:43 +0530

    Remove Nth Node From End Of List.

--- src/com/shraddha/AddTwoNumbers.java
+++ src/com/shraddha/AddTwoNumbers.java
@@ -1,36 +1,43 @@
 package com.shraddha;
 
 import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.LinkedList;
 
 class ListNode {
     int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    ListNodeDummy next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNodeDummy next) {
+        this.val = val;
+        this.next = next;
+    }
 }
 
 public class AddTwoNumbers {
-    public static void addTwoNumbers(ListNode l1, ListNode l2){
+    public static void addTwoNumbers(ListNodeDummy l1, ListNodeDummy l2) {
         String number1 = "";
         String number2 = "";
         String revNumber1 = "";
         String revNumber2 = "";
 
-        while(l1 != null){
-            number1 += l1.val+"";
+        while (l1 != null) {
+            number1 += l1.val + "";
             l1 = l1.next;
         }
-        while(l2 != null){
-            number2 += l2.val+"";
+        while (l2 != null) {
+            number2 += l2.val + "";
             l2 = l2.next;
         }
-        for(int i = number1.length() -1;i>=0;i--) revNumber1+=number1.charAt(i);
-        for(int i = number2.length() -1;i>=0;i--) revNumber2+=number2.charAt(i);
+        for (int i = number1.length() - 1; i >= 0; i--) revNumber1 += number1.charAt(i);
+        for (int i = number2.length() - 1; i >= 0; i--) revNumber2 += number2.charAt(i);
 
-        System.out.println("num1 = "+revNumber1+"\n"+"num2 = "+revNumber2);
+        System.out.println("num1 = " + revNumber1 + "\n" + "num2 = " + revNumber2);
 
         BigInteger b1 = new BigInteger(revNumber1);
         BigInteger b2 = new BigInteger(revNumber2);
@@ -40,22 +47,22 @@ public class AddTwoNumbers {
         // int n2 = Math.toIntExact(n1);
         BigInteger n1 = b1.add(b2);
 
-        String num = n1+"";
+        String num = n1 + "";
         String revNum = "";
-        for(int i = num.length() -1;i>=0;i--) revNum+=num.charAt(i);
+        for (int i = num.length() - 1; i >= 0; i--) revNum += num.charAt(i);
 
-        System.out.println("Num = "+num+"\n"+"rev num="+revNum);
+        System.out.println("Num = " + num + "\n" + "rev num=" + revNum);
 
-        int data = (Integer.parseInt(revNum.charAt(0)+""));
-        ListNode head = new ListNode(data);
-        ListNode n = head;
-        for(int i = 1;i<revNum.length();i++){
-            data = (Integer.parseInt(revNum.charAt(i)+""));
-            n.next = new ListNode(data);
+        int data = (Integer.parseInt(revNum.charAt(0) + ""));
+        ListNodeDummy head = new ListNodeDummy(data);
+        ListNodeDummy n = head;
+        for (int i = 1; i < revNum.length(); i++) {
+            data = (Integer.parseInt(revNum.charAt(i) + ""));
+            n.next = new ListNodeDummy(data);
             n = n.next;
         }
 
-        while (head!= null){
+        while (head != null) {
             System.out.print(head.val);
             head = head.next;
         }
@@ -64,33 +71,33 @@ public class AddTwoNumbers {
     }
 
     public static void main(String[] args) {
-        ListNode list1 = new ListNode(9);
+        ListNodeDummy list1 = new ListNodeDummy(9);
 //        ListNode l1 = list1;
 //        l1.next = new ListNode(4);
 //        l1 = l1.next;
 //        l1.next = new ListNode(9);
 
-        ListNode list2 = new ListNode(9);
-        ListNode l2 = list2;
-        l2.next = new ListNode(9);
+        ListNodeDummy list2 = new ListNodeDummy(9);
+        ListNodeDummy l2 = list2;
+        l2.next = new ListNodeDummy(9);
         l2 = l2.next;
-        l2.next = new ListNode(9);
+        l2.next = new ListNodeDummy(9);
         l2 = l2.next;
-        l2.next = new ListNode(9);
+        l2.next = new ListNodeDummy(9);
         l2 = l2.next;
-        l2.next = new ListNode(9);
+        l2.next = new ListNodeDummy(9);
         l2 = l2.next;
-        l2.next = new ListNode(9);
+        l2.next = new ListNodeDummy(9);
         l2 = l2.next;
-        l2.next = new ListNode(9);
+        l2.next = new ListNodeDummy(9);
         l2 = l2.next;
-        l2.next = new ListNode(9);
+        l2.next = new ListNodeDummy(9);
         l2 = l2.next;
-        l2.next = new ListNode(9);
+        l2.next = new ListNodeDummy(9);
         l2 = l2.next;
-        l2.next = new ListNode(1);
+        l2.next = new ListNodeDummy(1);
 
-        addTwoNumbers(list1,list2);
+        addTwoNumbers(list1, list2);
 
     }
 }
--- src/com/shraddha/RemoveNthNodeFromEndOfList.java
+++ src/com/shraddha/RemoveNthNodeFromEndOfList.java
@@ -1,2 +1,70 @@
-package com.shraddha;public class RemoveNthNodeFromEndOfList {
+package com.shraddha;
+
+class ListNodeDummy {
+    int val;
+    ListNodeDummy next;
+
+    ListNodeDummy() {
+    }
+
+    ListNodeDummy(int val) {
+        this.val = val;
+    }
+
+    ListNodeDummy(int val, ListNodeDummy next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class RemoveNthNodeFromEndOfList {
+    public static void printList(ListNodeDummy head) {
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+
+    public static int lengthOfList(ListNodeDummy head) {
+        int count = 0;
+        while (head != null) {
+            head = head.next;
+            count++;
+        }
+        return count;
+    }
+
+    public static ListNodeDummy removeNthFromEnd(ListNodeDummy head, int n) {
+        ListNodeDummy t = head;
+        int lengthOfList = lengthOfList(head);
+        System.out.println("length of list = " + lengthOfList);
+
+        int count = 1;
+        if (lengthOfList == 1) return null;
+        if (lengthOfList == n) return head.next;
+        while (count != (lengthOfList - n)) {
+            t = t.next;
+            count++;
+        }
+        t.next = t.next.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNodeDummy head;
+        ListNodeDummy n = new ListNodeDummy(5);
+        n = new ListNodeDummy(4, n);
+        n = new ListNodeDummy(3, n);
+        n = new ListNodeDummy(2, n);
+        head = new ListNodeDummy(1, n);
+
+//        ListNodeDummy n = new ListNodeDummy(2);
+//        head = new ListNodeDummy(1, n);
+
+        System.out.println("Original list = ");
+        printList(head);
+        ListNodeDummy ans = removeNthFromEnd(head, 4);
+        System.out.println("After deleting last nth node =");
+        printList(ans);
+    }
 }
