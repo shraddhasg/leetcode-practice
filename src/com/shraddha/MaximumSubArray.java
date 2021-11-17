8028d0391ef71f67fbec7a2eeb6ba7280472ef0f
Shraddha Shinde <shraddha.s@FinIQ-SP.com>
2021-11-17 18:25:31 +0530

    Maximumsubarray Sum and RotateImage arially done.

--- src/com/shraddha/MaximumSubArray.java
+++ src/com/shraddha/MaximumSubArray.java
@@ -1,2 +1,24 @@
-package com.shraddha;public class MaximumSubarray {
+package com.shraddha;
+
+import java.util.Arrays;
+
+public class MaximumSubArray {
+    public static int maxSubArray(int[] nums) {
+        int including_prev = nums[0];
+        int excluding_prev = 0;
+        int excluding_curr = 0;
+        for (int i = 1; i < nums.length; i++) {
+            excluding_curr = Math.max(including_prev, excluding_prev);
+            including_prev = excluding_prev + nums[i];
+            excluding_prev = excluding_curr;
+        }
+        return Math.max(including_prev, excluding_prev);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Input array = " + Arrays.toString(arr));
+        int maxSum = maxSubArray(arr);
+        System.out.println("Maximum sum = " + maxSum);
+    }
 }
--- src/com/shraddha/RotateImage.java
+++ src/com/shraddha/RotateImage.java
@@ -1,2 +1,25 @@
-package com.shraddha;public class RotateImage {
+package com.shraddha;
+
+import java.util.Arrays;
+
+public class RotateImage {
+    public static void rotate(int[][] matrix) {
+        int[][] temp = new int[matrix.length][matrix[0].length];
+        for (int i = matrix.length - 1; i >= 0; i--) {
+            for (int j = 0; j < matrix[i].length; j++) temp[j][i] = matrix[i][j];
+//            for (int j = matrix[i].length-1; j >=0 ; j--) temp
+
+        }
+        for (int i = 0; i < matrix.length; i++) matrix[i] = temp[i];
+
+        System.out.println("After rotation 2D array = ");
+        for (int[] a : matrix) System.out.println(Arrays.toString(a));
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println("Input 2D array = ");
+        for (int[] a : arr) System.out.println(Arrays.toString(a));
+        rotate(arr);
+    }
 }
